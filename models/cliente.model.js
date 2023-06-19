3ed06ed390a721781b37ab08140f437f050bf13d
Joan Marti <joanmartigamez@gmail.com [core]>
2023-06-19 04:19:48 +0200

    funciones getAll, getById y create hechas y sus peticiones

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -1,2 +1,17 @@
 //libreria de métodos que interan con la entidad clientes en la BD
 const tableDb = 'agencia_viajes_app.clientes';
+
+const getAll = () => {
+    return db.query(`SELECT * FROM ${tableDb}`);
+};
+
+const getById = (clienteId) => {
+    return db.query(`SELECT * FROM ${tableDb} WHERE id = ?`, [clienteId]);
+};
+
+const create = ({ nombre, apellidos, direccion, telefono, fecha_nacimiento, email, dni }) => {
+    return db.query(`INSERT INTO ${tableDb} ( nombre, apellidos, direccion, telefono, fecha_nacimiento, email, dni) VALUES ( ?, ?, ?, ?, ?, ?,? )`, [nombre, apellidos, direccion, telefono, fecha_nacimiento, email, dni]);
+};
+
+
+module.exports = { getAll, getById, create }
--- routes/api/clientes.js
+++ routes/api/clientes.js
@@ -1,12 +1,37 @@
 const router = require('express').Router();
+const { getAll, getById, create } = require('../../models/cliente.model');
+
+
+// Peticiones GET
 
 router.get('/', async (req, res) => {
     try {
-        const [result] = await db.query('SELECT * FROM agencia_viajes_app.clientes');
+        const [result] = await getAll();
         res.json(result);
     } catch (error) {
         res.json({ fatal: error.message });
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        let id = Number(req.params.id);
+        const [result] = await getById(id);
+        res.json(result);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+// Peticiones POST
+
+router.post('/', async (req, res) => {
+    try {
+        const [result] = await create(req.body);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
 
 module.exports = router; 
